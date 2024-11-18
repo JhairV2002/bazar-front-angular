51c84ecc654fdc5f758c84f78f64a2864e58335b
JhairV2002 <jhairvasquez2002@gmail.com>
2024-11-17 23:41:23 -0500

    fix products formArray

--- src/app/bills/pages/create-bill-page/create-bill-page.component.ts
+++ src/app/bills/pages/create-bill-page/create-bill-page.component.ts
@@ -12,6 +12,9 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
+import { ProductsService } from '../../../products/services/products.service';
+import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-create-bill-page',
@@ -24,16 +27,17 @@ import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
     MatCheckboxModule,
     MatIconModule,
     ReactiveFormsModule,
+    CommonModule,
   ],
   templateUrl: './create-bill-page.component.html',
 })
 export class CreateBillPageComponent {
   private fb: FormBuilder = inject(FormBuilder);
-  foods = [
-    { value: 'steak-0', viewValue: 'Steak' },
-    { value: 'pizza-1', viewValue: 'Pizza' },
-    { value: 'tacos-2', viewValue: 'Tacos' },
-  ];
+  private productService: ProductsService = inject(ProductsService);
+
+  products$ = this.productService
+    .getProducts()
+    .pipe(map((products) => products.data));
 
   billForm = this.fb.group({
     date: [''],
@@ -42,7 +46,7 @@ export class CreateBillPageComponent {
       this.fb.group({
         cant: [0],
         product: [''],
-        total: [0],
+        subtotal: [0],
       }),
     ]),
   });
@@ -60,12 +64,17 @@ export class CreateBillPageComponent {
       this.fb.group({
         cant: [0],
         product: [''],
-        total: [0],
+        subtotal: [0],
       })
     );
   }
 
   removeProduct(index: number) {
     this.products.removeAt(index);
+  }
+
+  saveBill() {
+    console.log('Bill saved');
+    console.log(this.billForm.value);
   }
 }
